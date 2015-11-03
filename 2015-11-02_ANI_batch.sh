d7d41c7f6c123b40f68333ef216d1c3e834d1884
Grace Shrader <dgshrader@zissou.bact.wisc.edu>
2015-11-02 21:06:00 -0600

    CLEAN working version of ANI batch calculator

--- 2015-11-02_ANI_batch.sh
+++ 2015-11-02_ANI_batch.sh
@@ -1,24 +1,4 @@
-ANI_PATH=/home/dgshrader/ANIcalculator_v1
-DATA_PATH=/home/dgshrader/data/Bacteroidetes_cds_1204/*
-NUM_FILES=ls -l $DATA_PATH|wc -l
-FILES_NAMES=ls $DATA_PATH
-
-#VERSION1
-for i in {1..NUM_FILES}; do
-for k in {1..NUM_FILES}; do
-./ANIcalculator -genome1fna ${i}.fna -genome2fna ${k}.fna -outfile testfile.txt -outdir /home/dgshrader/ANIoutput/
-done
-done
-
-#VERSION2
-for i in {2506381007..2506381009}; do
-for k in {2506381007..2506381009}; do
-./ANIcalculator -genome1fna $i -genome2fna ${k}.fna -outfile testfile.txt -outdir /home/dgshrader/ANIoutput/
-done
-done
-
-#VERSION3 THIS WORRRRRRRRRRRRRRRRRRKS!!!
-#PARAMETER to ENTER
+#PARAMETERS
 DATA_PATH=/home/dgshrader/data/test_data
 ANI_PATH=/home/dgshrader/ANIcalculator_v1
 #THE REST
