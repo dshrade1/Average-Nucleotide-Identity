87f642a90c30e29c5bedd6b70e1956618938b738
Grace Shrader <dgshrader@zissou.bact.wisc.edu>
2015-11-03 13:12:21 -0600

    suppressed screenout, show which calc performed

--- 2015-11-02_ANI_batch.sh
+++ 2015-11-02_ANI_batch.sh
@@ -2,7 +2,8 @@ DATA_PATH_FILES="$1"/*
 cd "$2"
 for i in $DATA_PATH_FILES; do
 for k in $DATA_PATH_FILES; do
-./ANIcalculator -genome1fna $i -genome2fna $k -outfile outfile20151102.txt -outdir /home/dgshrader/ANIoutput
+./ANIcalculator -genome1fna $i -genome2fna $k -outfile "$3"/ANI_batch_output.txt -outdir "$3"  > /dev/null
+echo "Calculating ANI between $i and $k"
 done
 done
 # The only thing is that this file does not save to the output directory 
