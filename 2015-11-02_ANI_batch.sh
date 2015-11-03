c78c357de2e53b362b2f3444537b7e8d8f82b82d
Grace Shrader <dgshrader@zissou.bact.wisc.edu>
2015-11-02 18:23:04 -0600

    ANI nested loop works now!

--- 2015-11-02_ANI_batch.sh
+++ 2015-11-02_ANI_batch.sh
@@ -17,12 +17,11 @@ for k in {2506381007..2506381009}; do
 done
 done
 
-#VERSION3
-#PARAMETERS
+#VERSION3 THIS WORRRRRRRRRRRRRRRRRRKS!!!
+#PARAMETER to ENTER
 DATA_PATH=/home/dgshrader/data/test_data
 ANI_PATH=/home/dgshrader/ANIcalculator_v1
-
-
+#THE REST
 DATA_PATH_FILES=$DATA_PATH/*
 cd $ANI_PATH
 for i in $DATA_PATH_FILES; do
@@ -30,3 +29,5 @@ for k in $DATA_PATH_FILES; do
 ./ANIcalculator -genome1fna $i -genome2fna $k -outfile outfile20151102.txt -outdir /home/dgshrader/ANIoutput
 done
 done
+# The only thing is that this file does not save to the output directory 
+# Instead, it always saves to a results file within ANIcalculator_v1 folder
