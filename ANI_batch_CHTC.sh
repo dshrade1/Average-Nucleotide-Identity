db0dc322cefb36e60b51ab198e5b31019a86fa0d
Grace Shrader <dgshrader@zissou.bact.wisc.edu>
2015-12-03 15:02:12 -0600

    place ANI scripts in ANI folder

--- ANI_batch_CHTC.sh
+++
@@ -1,6 +0,0 @@
-DATA_PATH_FILES="$1"/*
-for k in $DATA_PATH_FILES; do
-./ANIcalculator -genome1fna ${DATA_PATH_FILES[0]} -genome2fna $k
-done
-
-# ./ANI_batch_CHTC.sh /home/dgshrader/ANIcalculator_v1/ANI_data
