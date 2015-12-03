f4cf53e89db4952c3b178cc6bb03d40b61f96479
Grace Shrader <dgshrader@zissou.bact.wisc.edu>
2015-12-03 15:26:26 -0600

    remove files

--- test_ANI_batch.sh
+++
@@ -1,7 +0,0 @@
-DATA_PATH_FILES="$1"/*
-for i in $DATA_PATH_FILES; do
-for k in $DATA_PATH_FILES; do
-./ANIcalculator -genome1fna $i -genome2fna $k  -outdir "$3" >> test_results.txt
-echo "Calculating ANI between $i and $k"
-done
-done
--- test_ANI_batch2.sh
+++
@@ -1,7 +0,0 @@
-DATA_PATH_FILES="$1"/*
-for i in $DATA_PATH_FILES; do
-for k in $DATA_PATH_FILES; do
-./ANIcalculator -genome1fna $i -genome2fna $k
-done
-done
-# ./test_ANI_batch2.sh /home/dgshrader/ANIcalculator_v1/ANI_data
