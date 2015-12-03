ce4b9808cdf67687adb2cdcc1e3593712924d2a0
Grace Shrader <d.grace.shrader@gmail.com>
2015-12-03 16:45:23 -0600

    Update ANI_batch_CHTC.sh

--- ANI/ANI_batch_CHTC.sh
+++ ANI/ANI_batch_CHTC.sh
@@ -1,6 +1,20 @@
+# ./ANI_batch_CHTC.sh /home/dgshrader/ANIcalculator_v1/ANI_data
+
+#!/bin/bash
+#
+# ANI_batch_CHTC.sh
+# Pairwise ANI calculations
+#
+
+wget http://proxy.chtc.wisc.edu/SQUID/dgshrader/Bacteroidetes1205cds.tar.gz
+tar -xzvf Bacteroidetes1205cds.tar.gz
+
+rm Bacteroidetes1205cds.tar.gz
+#rm -r Bacteroidetes_cds_1204
+
 DATA_PATH_FILES="$1"/*
 for k in $DATA_PATH_FILES; do
-./ANIcalculator -genome1fna ${DATA_PATH_FILES[0]} -genome2fna $k
+./ANIcalculator -genome1fna $4 -genome2fna $k -outfile ANI_output_$2_$3
 done
 
-# ./ANI_batch_CHTC.sh /home/dgshrader/ANIcalculator_v1/ANI_data
+
